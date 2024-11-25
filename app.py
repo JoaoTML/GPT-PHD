232e7ba13ffdb3af99f9510d057816ed3507af50
kolimate <115666713+kolimate@users.noreply.github.com>
2024-11-25 19:54:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from chainlit.input_widget import Slider
 
 
 if "GROQ_API_KEY" not in os.environ:
-    os.environ["GROQ_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
+    #os.environ["GROQ_API_KEY"] = getpass.getpass("Enter your Groq API key: ")
 
 llm = ChatGroq(
     model="llama-3.1-70b-versatile",
